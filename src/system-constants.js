492fe9f4b8b2e47d73d98e256c4759c65392f70a
pmaric <petar.maric18@gmail.com>
2019-04-10 03:05:55 +0200

    added system-constants

---
+++ src/system-constants.js
@@ -0,0 +1,14 @@
+const userValueDefault = 0;
+
+const statuses = {
+    NEW: "new",
+    PENDING: "pending",
+    DENIED: "denied",
+    APPROVED: "approved",
+    DONE: "done"
+}
+
+module.exports = {
+    userValueDefault,
+    statuses
+}
