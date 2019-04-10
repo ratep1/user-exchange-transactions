08bf02a07db2a5ac7a13a803d8b4a385f60594b2
pmaric <petar.maric18@gmail.com>
2019-04-10 03:07:23 +0200

    added task-controller

---
+++ src/controllers/task-controller.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+
+const logger = require('../modules/logger');
+const { parseError } = require('../modules/utils/error-utility');
+
+const taskClusterController = require('../cluster/task-cluster-controller');
+
+const controllerResponses = require('./controller-responses');
+
+/**
+ * fetch list of all users from redis
+ */
+router.get('/', (req, res) => {
+    taskClusterController.fetchAll()
+    .then(entities => {
+        res.status(200).json(controllerResponses.entitiesResponse(entities));
+    })
+    .catch(error => {
+        const { code, message } = parseError(error);
+
+        res.status(200).json(controllerResponses.errorResponse(code, message));
+    });
+});
+
+/**
+ * create new task on redis
+ */
+router.post('/', (req, res) => {
+    const { id, name, value } = req.body;
+
+    taskClusterController.create(id, name, value)
+    .then(entity => {
+        res.status(200).json(controllerResponses.entityResponse(entity));
+    })
+    .catch(error => {
+        const { code, message } = parseError(error);
+
+        res.status(200).json(controllerResponses.errorResponse(code, message));
+    });
+});
+
+/**
+ * patch (update) only value for id on redis
+ */
+router.patch('/:id', (req, res) => {
+    const { id: payloadId, value } = req.body;
+    const { id: paramId } = req.params;
+
+    if(payloadId !== paramId) {
+        return res.status(200).json(controllerResponses.errorResponse(400, 'Invalid data'));
+    }
+
+    taskClusterController.updateValue(payloadId, value)
+    .then(entity => {
+        res.status(200).json(controllerResponses.entityResponse(entity));
+    })
+    .catch(error => {
+        const { code, message } = parseError(error);
+
+        res.status(200).json(controllerResponses.errorResponse(code, message));
+    });
+});
+
+module.exports = router;
