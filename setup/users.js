a1419087570e50b0bc3db2c1d734716bbd7e06bf
pmaric <petar.maric18@gmail.com>
2019-04-10 03:05:41 +0200

    added initialize users

---
+++ setup/users.js
@@ -0,0 +1,29 @@
+const users = [
+    {
+        "id": "1",
+        "value": 0,
+        "role": "admin"
+    },
+    {
+        "id": "2",
+        "value": 0,
+        "role": "agent"
+    },
+    {
+        "id": "3",
+        "value": 0,
+        "role": "agent"
+    },
+    {
+        "id": "4",
+        "value": 0,
+        "role": "agent"
+    },
+    {
+        "id": "5",
+        "value": 0,
+        "role": "agent"
+    }
+]
+
+module.exports = users;
