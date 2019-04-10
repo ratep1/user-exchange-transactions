3c1b175ea45695e392d49f1c9ce4fb6e3b08ee3b
pmaric <petar.maric18@gmail.com>
2019-04-10 03:05:33 +0200

    added initialize tasks

---
+++ setup/tasks.js
@@ -0,0 +1,19 @@
+const tasks = [
+    {
+        id: '1',
+        value: 10,
+        name: 'Go to coffe shop'
+    },
+    {
+        id: '2',
+        value: 50,
+        name: 'Go to deli'
+    },
+    {
+        id: '3',
+        value: 30,
+        name: 'Go to the cinema'
+    }
+]
+
+module.exports = tasks;
