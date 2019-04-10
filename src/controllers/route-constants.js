d4822365c43edefcbcc732aa94c2059b1bbfd98b
pmaric <petar.maric18@gmail.com>
2019-04-10 03:07:11 +0200

    added route-constnats

---
+++ src/controllers/route-constants.js
@@ -0,0 +1,11 @@
+const routes = {
+    main: '/api',
+    business: {
+        users: '/users',
+        tasks: '/tasks',
+        objectives: '/objectives',
+        transactions: '/transactions'
+    }
+}
+
+module.exports = routes;
