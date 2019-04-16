c99e2f8c7fdbfd5a0cfa88d2961518bf3e18648d
pmaric <petar.maric18@gmail.com>
2019-04-17 01:18:28 +0200

    system-constants added roles and default values

--- src/system-constants.js
+++ src/system-constants.js
@@ -1,4 +1,6 @@
 const userValueDefault = 0;
+const taskValueDefault = 10;
+const transactionValueDefault = 1;
 
 const statuses = {
     NEW: "new",
@@ -8,7 +10,22 @@ const statuses = {
     DONE: "done"
 }
 
+const roles = {
+    USER: 'user',
+    ADMIN: 'admin'
+}
+
+const checkStatus = status => {
+    return Object.values(statuses).includes(status);
+}
+
 module.exports = {
     userValueDefault,
-    statuses
+    taskValueDefault,
+    transactionValueDefault,
+
+    statuses,
+    checkStatus,
+
+    roles
 }
